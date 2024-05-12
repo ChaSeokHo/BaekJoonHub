49415ff6f61e0344c99baf570247d3ea7a2e90cb
ChaSeokHo <116864886+ChaSeokHo@users.noreply.github.com>
2024-05-12 19:42:05 +0900

    [Bronze III] Title: 최소, 최대, Time: 1700 ms, Memory: 318528 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr [] = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		int min = arr[0];
+		int max = arr[0];
+		for(int i = 0; i <arr.length; i++) {
+			if(min > arr[i]) {
+				min = arr[i];
+			}
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println(min+" "+max);
+	}
+}
